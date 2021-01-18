af5ef66c24a9a29809a55ff5017a07d73535bc9b
Nick Norcross <nicholas.norcross@gmail.com>
2021-01-18 12:44:34 -0500

    initial commit, version 1.0

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import Home from './components/pages/HomePage/Home';
+//import Blog from './components/pages/Blog/Blog';
+import About from './components/pages/About/About';
+import Contact from './components/pages/Contact/Contact';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Footer from './components/pages/Footer.js/Footer';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar />
+      <Switch>
+        <Route path='/' exact component={Home} />
+        {/*<Route path='/blog' component={Blog} />*/}
+        <Route path='/about' component={About} />
+        <Route path='/contact' component={Contact} />
+      </Switch>
+      <Footer />
+    </Router>
+
   );
 }
 
