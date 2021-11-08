7683223b461f3b267c2eff9ff086b591b47e67ed
Nick Norcross <nicholas.norcross@gmail.com>
2021-11-07 21:08:15 -0500

    Initial commit

---
+++ src/components/pages/Contact/Data.js
@@ -0,0 +1,14 @@
+import contactImg from '../../images/contact.png';
+export const contactObjOne = {
+    lightBg: false,
+    lightText: true,
+    lightTextDesc: true,
+    topLine: '',
+    headline: 'Contact Me',
+    description:
+      'Contact me via email at nicholas.norcross@gmail.com, or use the form below! I will get back to you as fast as I can.',
+    buttonLabel: 'Get Started',
+    imgStart: 'start',
+    img: contactImg,
+    alt: 'Contact'
+  };
---
+++ src/components/pages/HomePage/Data.js
@@ -0,0 +1,14 @@
+import inspiration from '../../images/inspiration.png';
+export const homeObjOne = {
+  lightBg: false,
+  lightText: true,
+  lightTextDesc: true,
+  topLine: '',
+  headline: 'Welcome',
+  description:
+    'Hey! 👋 I\'m Nick, a software developer who builds cool apps using React, Python, and containers. Currently, I\'m working on a social platform for students.',
+  buttonLabel: 'Contact Me',
+  imgStart: '',
+  img: inspiration,
+  alt: 'Inspiration'
+};
